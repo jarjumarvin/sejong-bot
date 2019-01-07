76e03f3a85d1402b7be736ba200df5bb1aeb3682
Marvin Jarju <marvin.jarju@gmail.com>
2019-01-07 10:05:07 +0100

    invite link escape

--- commands/invite.js
+++ commands/invite.js
@@ -1,10 +1,10 @@
 module.exports = {
   name: 'invite',
-  description: 'Get an invite link.',
+  description: 'Get an invite link to invite me to your server!',
   cooldown: 60,
   longdescription: '',
   execute(message) {
     return message.channel
-      .send(`Use https://discordapp.com/oauth2/authorize?client_id=${message.client.user.id}&scope=bot&permissions=52288 to invite me to your server.`);
+      .send(`Use <https://discordapp.com/oauth2/authorize?client_id=${message.client.user.id}&scope=bot&permissions=52288> to invite me to your server.`);
   },
 };
