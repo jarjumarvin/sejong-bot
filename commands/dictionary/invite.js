c7a374c38cace81a03c7464a1cc83505f4c97687
Mariana <marianagaldomartinez@gmail.com>
2022-02-13 11:17:22 -0300

    Update invite.js

--- commands/dictionary/invite.js
+++ commands/dictionary/invite.js
@@ -1,10 +1,21 @@
-module.exports = {
-  name: 'invite',
-  description: 'Get an invite link to invite me to your server!',
-  cooldown: 60,
-  longdescription: '',
-  execute(message) {
+const DiscordUtil = require('../../common/discordutil');
+const { Command } = require('discord.js-commando');
+
+
+module.exports = class DictionaryCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'invite',
+      cooldown: 3,
+      group: 'dictionary',
+      aliases: ['inv', 'i'],
+      memberName: 'invite',
+      description: 'Get an invite link for the bot',
+    })
+  }
+
+  run(message) {
     return message.channel
-      .send(`Use <https://discordapp.com/oauth2/authorize?client_id=${message.client.user.id}&scope=bot&permissions=60480> to invite me to your server.`);
-  },
+    .send(`Use <https://discordapp.com/oauth2/authorize?client_id=${message.client.user.id}&scope=bot&permissions=60480> to invite me to your server.`);
+  }
 };
