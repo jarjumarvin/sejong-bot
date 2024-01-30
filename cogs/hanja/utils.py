7bc0382e7ae95071ad86bd6d911175c28b6cf7fd
Marvin Jarju <marvin.jarju@gmail.com>
2024-01-30 18:01:11 +0100

    hanja display

--- cogs/hanja/utils.py
+++ cogs/hanja/utils.py
@@ -30,13 +30,13 @@ HANJA_QUERY_1 = """
         SELECT hanja, hangul, english 
         FROM hanjas 
         WHERE hidden_index MATCH {query} 
-        UNION (
-            SELECT hanja, hangul, english FROM hanjas WHERE english match {query}
-            UNION
-            SELECT hanja, hangul, english FROM hanjas WHERE hangul match {query}
-            UNION
-            SELECT hanja, hangul, english FROM hanjas WHERE hanjas match {query}
-        )
+        UNION
+        SELECT hanja, hangul, english FROM hanjas WHERE english match {query}
+        UNION
+        SELECT hanja, hangul, english FROM hanjas WHERE hangul match {query}
+        UNION
+        SELECT hanja, hangul, english FROM hanjas WHERE hanjas match {query}
+    )
     ORDER BY hangul
 """
 
@@ -48,11 +48,10 @@ HANJA_QUERY_2 = """
 
 
 async def get_hanja(query: str):
-    query = f"'{query.strip()}'"
     async with aiosqlite.connect(settings["sql_file"]) as db:
-        result = HanjaResult(query=query)
+        result = HanjaResult(query=f"'{query.strip()}'")
 
-        q1 = await db.execute(HANJA_QUERY_1.format(query=query))
+        q1 = await db.execute(HANJA_QUERY_1.format(query=f"'{query.strip()}'"))
 
         similar_words = await q1.fetchall()
 
@@ -60,7 +59,7 @@ async def get_hanja(query: str):
             HanjaWord(hanja=sw[0], hangul=sw[1], english=sw[2]) for sw in similar_words
         ]
 
-        for c in query:
+        for c in query.strip():
             q2 = await db.execute(HANJA_QUERY_2.format(character=c))
 
             hanjas = await q2.fetchall()
@@ -73,38 +72,54 @@ async def get_hanja(query: str):
 
 
 def get_hanja_embeds(result: HanjaResult):
-    l = result.similar_words + result.hanjas
+    l = result.hanjas + result.similar_words
 
     embeds = []
 
     if len(l) == 0:
         embed = discord.Embed(
             type="rich",
-            title=f"Hanja Results for **{result.query}**",
+            title=f"Hanja Search: **{result.query}**",
             color=settings["accent"],
         )
 
         return embed
 
+    # return the pages one by one
     for i in range(0, len(l), 10):
         sub = l[i : i + 10]
 
         embed = discord.Embed(
             type="rich",
-            title=f"Hanja Results for **{result.query}**",
+            title=f"Hanja Search: **{result.query}**",
             color=settings["accent"],
         )
 
+        seen_similar = False
+        seen_character = False
+
         for element in sub:
-            if isinstance(element, HanjaDefinition):
-                embed.add_field(
-                    name=element.hanja, value=element.definition, inline=False
-                )
-            elif isinstance(element, HanjaWord):
+            if isinstance(element, HanjaWord):
+                if not seen_similar:
+                    # for the first one
+                    embed.add_field(name="> Matching Words", value="", inline=False)
+                    seen_similar = True
+
                 embed.add_field(
                     name=f"{element.hanja} ({element.hangul})",
                     value=element.english,
                     inline=False,
+                )
+            elif isinstance(element, HanjaDefinition):
+                if not seen_character:
+                    # for the first one
+                    embed.add_field(
+                        name="> Matching Characters", value="", inline=False
+                    )
+                    seen_character = True
+
+                embed.add_field(
+                    name=element.hanja, value=element.definition, inline=False
                 )
 
         embeds.append(embed)
